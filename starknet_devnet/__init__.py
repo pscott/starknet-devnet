20b040eee1d5305c9aeb3ce5c6f7f33600d5457d
FabijanC <fabijan.corak@gmail.com>
2023-07-03 13:27:02 +0200

    Bump version to v0.5.5a0
      * [skip ci] - forking tests failing - testnet not yet updated
    
      * doing a manual relrease

--- starknet_devnet/__init__.py
+++ starknet_devnet/__init__.py
@@ -13,7 +13,7 @@ This file contains monkeypatches used across the project. Advice for monkeypatch
 import os
 import sys
 
-__version__ = "0.5.4"
+__version__ = "0.5.5a0"
 
 
 def _patch_pedersen_hash():
